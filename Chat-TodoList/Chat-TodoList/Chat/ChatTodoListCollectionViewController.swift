0b38d80e8877988e8556264f05af46c37a0381ab
kanghuiseon <ekfsla0113@naver.com>
2021-11-22 03:18:44 +0900

    navigation title view추가 : 일반적인, Todo List의 폰트 색을 다르게 하기 위하기 위해서 각각의 레이블을 가진 뷰를 생성하고 스택 뷰에 추가

--- Chat-TodoList/Chat-TodoList/Chat/ChatTodoListCollectionViewController.swift
+++ Chat-TodoList/Chat-TodoList/Chat/ChatTodoListCollectionViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 class ChatTodoListCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.topItem?.titleView = setTitleView()
     }
-
+    func setTitleView() -> UIView{
+        let firstView = UIView()
+        let firstLabel = UILabel()
+        firstView.addSubview(firstLabel)
+        firstLabel.text = "일반적인"
+        firstLabel.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize, weight: .bold)
+        firstLabel.textColor = .blue
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            firstLabel.leadingAnchor.constraint(equalTo: firstView.leadingAnchor),
+            firstLabel.trailingAnchor.constraint(equalTo: firstView.trailingAnchor),
+            firstLabel.topAnchor.constraint(equalTo: firstView.topAnchor),
+            firstLabel.bottomAnchor.constraint(equalTo: firstView.bottomAnchor),
+        ])
+        
+        let secondView = UIView()
+        let secondLabel = UILabel()
+        secondView.addSubview(secondLabel)
+        secondLabel.text = "Todo List"
+        secondLabel.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize, weight: .bold)
+        secondLabel.textColor = .black
+        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            secondLabel.leadingAnchor.constraint(equalTo: secondView.leadingAnchor),
+            secondLabel.trailingAnchor.constraint(equalTo: secondView.trailingAnchor),
+            secondLabel.topAnchor.constraint(equalTo: secondView.topAnchor),
+            secondLabel.bottomAnchor.constraint(equalTo: secondView.bottomAnchor),
+            
+        ])
+        let stackView = UIStackView(arrangedSubviews: [firstView, secondView])
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        return stackView
+    }
 }
